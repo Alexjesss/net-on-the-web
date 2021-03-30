02872f088355a36abd0a9667375cb3e6396e6d73
Alexandra Banica <banica9221@hotmail.be>
2021-03-30 16:15:39 +0200

    basic structure, models, student class,etc

---
+++ Netonweb/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Netonweb.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ClassId { get; set; }
+    }
+}
